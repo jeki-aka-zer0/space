77e67132baaf80cee58a92624c46588f10f34d77
zero <casbetgaming7@gmail.com>
2019-05-06 16:25:41 +0300

    Front: temporary code for scroll

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,19 +1,22 @@
 <template>
-<!--    <div class="page-home display-flex max-width-center">-->
-<!--        <div class="flex-xs-12">-->
-        <div class="pages-wrapper">
-            <Greeting/>
-            <About/>
-            <Values/>
-            <Projects/>
-            <Jobs/>
-            <Contacts/>
-        </div>
-<!--        </div>-->
-<!--    </div>-->
+    <!--    <div class="page-home display-flex max-width-center">-->
+    <!--        <div class="flex-xs-12">-->
+
+    <VueCustomScrollbar class="pages-wrapper" :settings="scrollBarSettings">
+        <Greeting/>
+        <About/>
+        <Values/>
+        <Projects/>
+        <Jobs/>
+        <Contacts/>
+    </VueCustomScrollbar>
+
+    <!--        </div>-->
+    <!--    </div>-->
 </template>
 
 <script>
+    import VueCustomScrollbar from 'vue-custom-scrollbar'
     import Greeting from '@/components/pages/Greeting.vue'
     import About from '@/components/pages/About.vue'
     import Values from '@/components/pages/Values.vue'
@@ -22,8 +25,17 @@
     import Contacts from '@/components/pages/Contacts.vue'
 
     export default {
-        name: 'home',
+        data() {
+            return {
+                scrollBarSettings: {
+                    useBothWheelAxes: true,
+                    minScrollbarLength: 50,
+                    maxScrollbarLength: 50
+                }
+            };
+        },
         components: {
+            VueCustomScrollbar,
             Greeting,
             About,
             Values,
@@ -40,7 +52,33 @@
         flex-wrap: nowrap;
         overflow-x: scroll;
         overflow-y: hidden;
-        margin: 40px 0;
+        margin: 40px 0 0;
+    }
+
+    .ps {
+        & > .ps__rail-x, &:hover > .ps__rail-x {
+            height: 60px;
+            opacity: 1;
+            background: transparent !important;
+
+            &:after {
+                content: '';
+                background: red;
+                display: block;
+                height: 3px;
+                margin-top: 30px;
+            }
+        }
+    }
+
+
+    /*.ps__rail-x:hover .ps__thumb-x,*/
+
+    .ps__thumb-x {
+        /*background: url("https://banner2.kisspng.com/20180202/zjq/kisspng-rocket-icon-rocket-5a7428c2d551a6.1639587815175620508738.jpg");*/
+        background: red !important;
+        width: 100px !important;
+        height: 60px !important;
     }
 
     .page {
